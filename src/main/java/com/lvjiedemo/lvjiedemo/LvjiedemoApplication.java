005c95cd4bf615c9071df942cd85ddaac469c45f
LV <877662951@qq.com>
2020-09-25 10:00:56 +0800

    Initial commit

---
+++ src/main/java/com/lvjiedemo/lvjiedemo/LvjiedemoApplication.java
@@ -0,0 +1,13 @@
+package com.lvjiedemo.lvjiedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvjiedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LvjiedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lvjiedemo/lvjiedemo/LvjiedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvjiedemo.lvjiedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvjiedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
